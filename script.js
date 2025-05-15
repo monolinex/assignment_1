e986b717f656b69cc94429b0c7010f9542c74c95
monolinex <rohan.lnu.2400679@gmail.com>
2025-05-15 08:57:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function handleClick() {
-    alert("Thanks for clicking!");
-   }
-   document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Form submitted! (This is a demo. Backend not connected.)");
-   });
